2b2df1ec2fac388494a7f50ad5ef5163330bff64
foadsdt <foad.sdt@gmail.com>
2025-02-15 19:16:12 +0330

    Add Theme Entity, Repository, and Migration - Add Theme entity with necessary fields - Create Theme repository for database interactions - Generate migration for Theme table

---
+++ api/src/Entity/Theme.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: "App\Repository\ThemeRepository")]
+#[ApiResource]
+#[ApiFilter(BooleanFilter::class, properties: ['isDefault'])]
+#[ApiFilter(SearchFilter::class, properties: ['name' => 'partial'])]
+class Theme
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    private string $name;
+
+    #[ORM\Column(type: "json", nullable: true)]
+    private ?array $colors = null;
+
+    #[ORM\Column(type: "boolean")]
+    private bool $isDefault = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getColors(): ?array
+    {
+        return $this->colors;
+    }
+
+    public function setColors(?array $colors): self
+    {
+        $this->colors = $colors;
+
+        return $this;
+    }
+
+    public function getIsDefault(): bool
+    {
+        return $this->isDefault;
+    }
+
+    public function setIsDefault(bool $isDefault): self
+    {
+        $this->isDefault = $isDefault;
+
+        return $this;
+    }
+}
---
+++ api/src/Repository/ThemeRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+// src/Repository/ThemeRepository.php
+namespace App\Repository;
+
+use App\Entity\Theme;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Theme|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Theme|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Theme[]    findAll()
+ * @method Theme[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ThemeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Theme::class);
+    }
+
+    // Custom query methods for Theme entity can be added here.
+
+}
