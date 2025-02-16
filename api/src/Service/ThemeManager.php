2442b1ed375ed79f66f751eec6ba27f66d87a0d9
foadsdt <foad.sdt@gmail.com>
2025-02-16 14:22:00 +0330

    Add ThemeManager Service, - Add ThemeManager to handle isDefault to include in both POST and PATCH requests

---
+++ api/src/Service/ThemeManager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Theme;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ThemeManager
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    public function handleIsDefault(Theme $theme): void
+    {
+        $existingDefaultTheme = $this->em->getRepository(Theme::class)->findOneBy(['isDefault' => true]);
+
+        if ($theme->getIsDefault()) {
+            if ($existingDefaultTheme && $existingDefaultTheme !== $theme) {
+                $existingDefaultTheme->setIsDefault(false);
+            }
+        } else {
+            if (!$existingDefaultTheme) {
+                $theme->setIsDefault(true);
+            }
+        }
+
+        $this->em->flush();
+    }
+}
